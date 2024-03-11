23a17cc016d076816296ba2095e0d7029edbabd6
AnoopVL <avlanjekar4@gmail.com>
2024-03-12 01:39:37 +0530

    ds-info

---
+++ app/src/main/java/com/avl/ahendriver/dsInfo.java
@@ -0,0 +1,14 @@
+package com.avl.ahendriver;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class dsInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ds_info);
+    }
+}
