fbd26dd0ebb606f821053ccffe4a7b9af6b41aa5
AnoopVL <avlanjekar4@gmail.com>
2024-03-13 02:22:51 +0530

    intent

--- app/src/main/java/com/avl/ahendriver/MainActivity.java
+++ app/src/main/java/com/avl/ahendriver/MainActivity.java
@@ -2,12 +2,14 @@ package com.avl.ahendriver;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.animation.Animation;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -17,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
 //    Animation topAnim, bottomAnim;
 //    ImageView drivingSchool;
 //    TextView ahen, solgan;
+    private Button regBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +31,17 @@ public class MainActivity extends AppCompatActivity {
             ((Window) window).getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
             window.setStatusBarColor(Color.TRANSPARENT);
         }
+
+        regBtn = findViewById(R.id.regBtn);
+        regBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, registerSchool.class);
+                startActivity(intent);
+            }
+        });
+
+
 //        topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation);
 //        bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
 
